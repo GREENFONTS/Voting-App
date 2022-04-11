31bb7956e9b4717421e5bc505b822f12ba07e42c
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-04-11 13:25:17 +0100

    cleared some files

--- services/sha.js
+++
@@ -1,7 +0,0 @@
-const sha1 = require('sha-1')
-
-export function sha1Generator(payload_to_sign) {
-    console.log(process.env.API_SECRET, 'enetred')
-    var signature = sha1(payload_to_sign, process.env.API_SECRET)
-    return signature
-}
