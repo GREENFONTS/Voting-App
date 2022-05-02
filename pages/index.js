1306972f99af932edadba88a59c8dd36fc2bd187
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-05-02 18:16:36 +0100

    cleared html

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,6 @@ export default function Home() {
   
   return (
     <>
-    <head>
-      <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-  <meta name="description" content="description of your project" />
-  <meta name="theme-color" content="#000" />
-  <title>Title of the project</title>
-  <link rel="manifest" href="/manifest.json" />
-  <link rel="shortcut icon" href="/favicon.ico" />
-  <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-</head>
 
     <Box>
       {isLargerThan900 && <Center h='90vh' w='100%'>
