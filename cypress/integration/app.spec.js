210e966bfed2badc7e90adb18295846888dac7aa
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-05-02 18:21:01 +0100

    pwa done

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -36,13 +36,13 @@ describe('Navigation', () => {
       cy.get('button').contains('Sign In with Google')
 
       //for testing logging in with google button
-      let google_button =  cy.get('button').contains('Sign In with Google')
-      google_button.click()
+      // let google_button =  cy.get('button').contains('Sign In with Google')
+      // google_button.click()
 
       //logging in with email and password
-      // cy.get('input[id=email]').type('godwillonyewuchi@yahoo.com')
-      // cy.get('input[id=password]').type('comfort9')
-      // cy.get('button[id=button]').click()
+      cy.get('input[id=email]').type('godwillonyewuchi@yahoo.com')
+      cy.get('input[id=password]').type('comfort9')
+      cy.get('button[id=button]').click()
 
       //redirect to dashboard and testing dashboard
       cy.contains('Welcome to your dashboard')
