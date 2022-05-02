8bf3c9686fc118df29510e2733dd67ff0f86062a
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-05-02 18:29:56 +0100

    head tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Head from 'next/head'
+import Head from 'next/head';
 import { Box,Text,Button,  useMediaQuery, Image, Center} from '@chakra-ui/react';
 import { useColorModeValue } from '@chakra-ui/react';
 import Bounce from 'react-reveal/Bounce';
@@ -15,7 +15,7 @@ export default function Home() {
     <>
     <Head>
       <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
   <meta name="description" content="description of your project" />
   <meta name="theme-color" content="#000" />
