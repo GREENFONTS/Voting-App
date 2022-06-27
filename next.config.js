f2bb5bb4dc88d878eacdd461b814697c38870d4e
GREEENFONTS <58170214+GREENFONTS@users.noreply.github.com>
2022-06-27 22:53:20 +0100

    removed env variables

--- next.config.js
+++ next.config.js
@@ -13,14 +13,5 @@ withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
-  },
-  env: {
-
-      TOKEN_KEY : "comfort9",
-
-      CLOUD_NAME : "elevio",
-      API_KEY : "441632273511282",
-      API_SECRET : "QZ_pdgzLjbs90_CinCMIkUYAxis"
   }
-
 })
