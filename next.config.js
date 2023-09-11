752f96b9c169136ae3fc2f8b07a3907cc75897c8
GREENFONTS <godwillonyewuchii@gmail.com>
2023-09-11 07:58:47 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
- 
-}
-
-module.exports = {
-  distDir: "out",
-  reactStrictMode: true
 };
+
+module.exports = nextConfig;
