76e7545def4aded4bb1a1259bcc55ecdbeb8a2a5
GREENFONTS <godwillonyewuchii@gmail.com>
2023-09-11 07:50:58 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
 const nextConfig = {
   reactStrictMode: true,
+ 
 }
-// module.exports = withPWA({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === "development",
-//   register: true,
-//   scope: "/app",
-//   sw: "service-worker.js",
-//   // next.js config
-// });
 
-module.exports = nextConfig;
+module.exports = {
+  distDir: "out",
+  reactStrictMode: true
+};
