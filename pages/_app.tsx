fb523ed980515bd98b8d97027286c65e5445b8f2
GREENFONTS <godwillonyewuchii@gmail.com>
2023-09-11 08:09:16 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ const MyApp: NextPage<RootProps> = ({
       <CacheProvider value={emotionCache}>
         <ChakraProvider>
           <Layout>
-            <Component {...(pageProps || {})} />
+            <Component {...pageProps} />
           </Layout>
         </ChakraProvider>
       </CacheProvider>
